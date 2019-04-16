d29ea5f62c7263028add64ec9c560d1994d13ab6
Ravi3114 <guggilapuravi3114@gmail.com>
2019-04-16 18:43:52 +0530

    Introduced Modelmap and Request method

--- springmvcfirst/src/main/java/com/mycompany/springmvc/LoginController.java
+++ springmvcfirst/src/main/java/com/mycompany/springmvc/LoginController.java
@@ -1,14 +1,22 @@
 package com.mycompany.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 	
-	@RequestMapping("/login")
-	public String sayHello() {
+	@RequestMapping(value="/login",method=RequestMethod.GET)
+	public String showLogin() {
 		return "Login";
+	}
+	@RequestMapping(value="",method=RequestMethod.POST)
+	public String handleLogin(@RequestParam String name,ModelMap model) {
+		model.put("name",name);
+		return "welcome";
 	}
 
 }
