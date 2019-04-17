ec99a7f58d6b7d34d4768da61debe3b934330e87
Ravi3114 <guggilapuravi3114@gmail.com>
2019-04-17 16:11:06 +0530

    Services also given to Handler using spring autowired

--- springmvcfirst/src/main/java/com/mycompany/springmvc/LoginService.java
+++ springmvcfirst/src/main/java/com/mycompany/springmvc/LoginService.java
@@ -1,5 +1,8 @@
 package com.mycompany.springmvc;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 	public boolean validateUser(String user, String password) {
 		return user.equalsIgnoreCase("Ravi3114") && password.equals("I@monly5");
